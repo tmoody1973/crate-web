a386769969225ab916db2f0ee93d223bc81f6946
Tarik Moody <tarikjmoody@gmail.com>
2026-03-24 14:07:13 -0500

    feat: add audio player to published Deep Cuts so anyone can listen

--- src/app/cuts/[shareId]/share-renderer.tsx
+++ src/app/cuts/[shareId]/share-renderer.tsx
@@ -2,7 +2,18 @@
 
 import { Renderer } from "@openuidev/react-lang";
 import { crateLibrary } from "@/lib/openui/library";
+import { PlayerProvider } from "@/components/player/player-provider";
+import { PlayerBar } from "@/components/player/player-bar";
+import { YouTubePlayer } from "@/components/player/youtube-player";
 
 export function ShareRenderer({ data }: { data: string }) {
-  return <Renderer library={crateLibrary} response={data} isStreaming={false} />;
+  return (
+    <PlayerProvider>
+      <Renderer library={crateLibrary} response={data} isStreaming={false} />
+      <div className="fixed bottom-0 left-0 right-0 z-40">
+        <PlayerBar />
+      </div>
+      <YouTubePlayer />
+    </PlayerProvider>
+  );
 }
